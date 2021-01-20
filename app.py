92607d6e46bceade3541e694da4beb2737a34e69
macareen <66545374+macareen@users.noreply.github.com>
2021-01-20 17:48:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ df=df_ev
 
 ########### Initiate the app
 #external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-external_stylesheets = ['https://codepen.io/chriddyp/pen/dZVMbK.css']
+external_stylesheets = ['https://codepen.io/saisubrahmanyamjanapati/pen/WNNOYLZ.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
